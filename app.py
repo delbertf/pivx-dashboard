808cd5f13f2d68fab45ada352dc917ed440f4d94
dfriesen <friesen.delbert@gmail.com>
2020-06-12 19:15:23 +0200

    use rpc to get information from pivxd

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, jsonify
-import subprocess
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import os
+
 app = Flask(__name__)
+RPC_USER = os.environ.get("RPC_USER")
+RPC_KEY = os.environ.get("RPC_KEY")
+
+if not RPC_USER:
+    raise ValueError("No RPC_USER set for Flask application")
+if not RPC_KEY:
+    raise ValueError("No RPC_KEY set for Flask application")
 
 
 @app.route('/')
@@ -15,4 +24,7 @@ def wallet():
 
 @app.route('/control/getinfo', methods=['POST'])
 def control_getinfo():
-    return jsonify({"info": "here your pivx"})
+    access = AuthServiceProxy(
+        "http://%s:%s@127.0.0.1:51473" % (RPC_USER, RPC_KEY))
+    print(access.getinfo())
+    return jsonify(access.getinfo())
