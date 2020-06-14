5be9e033b833363b7983136659b992dfb6c99b23
dfriesen <friesen.delbert@gmail.com>
2020-06-14 19:57:49 +0200

    add sync information card to dashboard

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def index():
     get_info = pivx_get_info()
     staking_status = pivx_get_staking_status()
     wallet_info = pivx_get_wallet_info()
-    return render_template('index.html', get_info=get_info, staking_status=staking_status, wallet_info=wallet_info, last_update=time.strftime("%Y-%m-%d %H:%M:%S", ts))
+    mnsync_status = pivx_get_mnsync_status()
+    return render_template('index.html', get_info=get_info, staking_status=staking_status, wallet_info=wallet_info, mnsync_status=mnsync_status, last_update=time.strftime("%Y-%m-%d %H:%M:%S", ts))
 
 
 @ app.route('/wallet')
@@ -32,13 +33,13 @@ def wallet():
 """
 
 
-@ app.route('/control/getinfo', methods=['POST'])
+@app.route('/control/getinfo', methods=['POST'])
 def control_getinfo():
     ts = time.gmtime()
     return jsonify({'data': render_template('index_getinfo.html', get_info=pivx_get_info(), last_update=time.strftime("%Y-%m-%d %H:%M:%S", ts))})
 
 
-@ app.route('/control/getstakingstatus', methods=['POST'])
+@app.route('/control/getstakingstatus', methods=['POST'])
 def control_getstakingstatus():
     ts = time.gmtime()
     return jsonify({'data': render_template('index_getstakinginfo.html', staking_status=pivx_get_staking_status(), last_update=time.strftime("%Y-%m-%d %H:%M:%S", ts))})
@@ -48,6 +49,12 @@ def control_getstakingstatus():
 def control_getwalletinfo():
     ts = time.gmtime()
     return jsonify({'data': render_template('index_getwalletinfo.html', wallet_info=pivx_get_wallet_info(), last_update=time.strftime("%Y-%m-%d %H:%M:%S", ts))})
+
+
+@app.route('/control/getmnsyncstatus', methods=['POST'])
+def control_getmnsyncstatus():
+    ts = time.gmtime()
+    return jsonify({'data': render_template('index_getmnsyncstatus.html', mnsync_status=pivx_get_mnsync_status(), last_update=time.strftime("%Y-%m-%d %H:%M:%S", ts))})
 
 
 """
@@ -73,7 +80,7 @@ def pivx_get_staking_status():
 
     include_properties = ["staking_status", "staking_enabled",
                           "coldstaking_enabled", "haveconnections", "mnsync", "walletunlocked", "stakeablecoins", "stakingbalance", "stakesplitthreshold"]
-    print(access.getstakingstatus())
+
     get_info = {key: value
                 for (key, value) in access.getstakingstatus().items()
                 if key in include_properties}
@@ -86,8 +93,21 @@ def pivx_get_wallet_info():
 
     include_properties = ["balance", "delegated_balance", "cold_staking_balance", "unconfirmed_balance",
                           "immature_balance", "immature_delegated_balance", "immature_cold_staking_balance", "txcount", "paytxfee"]
-    print(access.getwalletinfo())
+
     get_info = {key: value
                 for (key, value) in access.getwalletinfo().items()
+                if key in include_properties}
+    return get_info
+
+
+def pivx_get_mnsync_status():
+    access = AuthServiceProxy(
+        "http://%s:%s@127.0.0.1:51473" % (RPC_USER, RPC_KEY))
+
+    include_properties = ["IsBlockchainSynced", "lastMasternodeList",
+                          "lastMasternodeWinner", "lastFailure", "nCountFailures", "countMasternodeList"]
+
+    get_info = {key: value
+                for (key, value) in access.mnsync("status").items()
                 if key in include_properties}
     return get_info
