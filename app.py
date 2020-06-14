28d516c51edb1a233bdc6035c77469a30d53e0e0
dfriesen <friesen.delbert@gmail.com>
2020-06-14 17:40:19 +0200

    improve dashboard with partial refresh feature

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, jsonify
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import os
+import time
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 RPC_USER = os.environ.get("RPC_USER")
 RPC_KEY = os.environ.get("RPC_KEY")
 
@@ -14,17 +15,57 @@ if not RPC_KEY:
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    ts = time.gmtime()
+    return render_template('index.html', get_info=pivx_get_info(), staking_status=pivx_get_staking_status(), last_update=time.strftime("%Y-%m-%d %H:%M:%S", ts))
 
 
-@app.route('/wallet')
+@ app.route('/wallet')
 def wallet():
     return render_template('wallet.html')
 
 
-@app.route('/control/getinfo', methods=['POST'])
+"""
+ Application API
+"""
+
+
+@ app.route('/control/getinfo', methods=['POST'])
 def control_getinfo():
+    ts = time.gmtime()
+    return jsonify({'data': render_template('index_getinfo.html', get_info=pivx_get_info(), last_update=time.strftime("%Y-%m-%d %H:%M:%S", ts))})
+
+
+@ app.route('/control/getstakingstatus', methods=['POST'])
+def control_getstakingstatus():
+    ts = time.gmtime()
+    return jsonify({'data': render_template('index_getstakinginfo.html', staking_status=pivx_get_staking_status(), last_update=time.strftime("%Y-%m-%d %H:%M:%S", ts))})
+
+
+"""
+PIVX wrapper API functions
+"""
+
+
+def pivx_get_info():
     access = AuthServiceProxy(
         "http://%s:%s@127.0.0.1:51473" % (RPC_USER, RPC_KEY))
-    print(access.getinfo())
-    return jsonify(access.getinfo())
+
+    include_properties = ["version", "walletversion",
+                          "services", "balance", "staking status", "zerocoinbalance", "blocks", "connections", "testnet", "errors"]
+    get_info = {key: value
+                for (key, value) in access.getinfo().items()
+                if key in include_properties}
+    return get_info
+
+
+def pivx_get_staking_status():
+    access = AuthServiceProxy(
+        "http://%s:%s@127.0.0.1:51473" % (RPC_USER, RPC_KEY))
+
+    include_properties = ["staking_status", "staking_enabled",
+                          "coldstaking_enabled", "haveconnections", "mnsync", "walletunlocked", "stakeablecoins", "stakingbalance", "stakesplitthreshold"]
+    print(access.getstakingstatus())
+    get_info = {key: value
+                for (key, value) in access.getstakingstatus().items()
+                if key in include_properties}
+    return get_info
