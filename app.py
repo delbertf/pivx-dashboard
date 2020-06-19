f59436764aa122ef58f491a403ce17a98b00ae24
dfriesen <friesen.delbert@gmail.com>
2020-06-19 23:02:45 +0200

    refactor pivxd server connection as decorator

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ import time
 app = Flask(__name__, static_url_path='')
 RPC_USER = os.environ.get("RPC_USER")
 RPC_KEY = os.environ.get("RPC_KEY")
+RPC_URL = os.environ.get("RPC_URL")
 
 if not RPC_USER:
     raise ValueError("No RPC_USER set for Flask application")
 if not RPC_KEY:
     raise ValueError("No RPC_KEY set for Flask application")
+if not RPC_URL:
+    raise ValueError("No RPC_URL set for Flask application")
 
 
 @app.route('/')
@@ -58,56 +61,68 @@ def control_getmnsyncstatus():
 
 
 """
+Decorators
+"""
+
+
+def access_pivxd(func):
+    """
+    Decorator function to access the pivxd server and create an access object
+    """
+    def inner(*args, **kwargs):
+        pivxd = AuthServiceProxy("http://%s:%s@%s" %
+                                 (RPC_USER, RPC_KEY, RPC_URL))
+        return func(pivxd)
+    return inner
+
+
+"""
 PIVX wrapper API functions to query the Pivx Server with json RPC
 """
 
 
-def pivx_get_info():
-    access = AuthServiceProxy(
-        "http://%s:%s@127.0.0.1:51473" % (RPC_USER, RPC_KEY))
-
+@access_pivxd
+def pivx_get_info(pivxd):
+    """
+    This function gets the basic information about the pivx wallet
+    """
     include_properties = ["version", "walletversion",
                           "services", "balance", "staking status", "zerocoinbalance", "blocks", "connections", "testnet", "errors"]
-    get_info = {key: value
-                for (key, value) in access.getinfo().items()
-                if key in include_properties}
-    return get_info
+    return {key: value
+            for (key, value) in pivxd.getinfo().items()
+            if key in include_properties}
 
 
-def pivx_get_staking_status():
-    access = AuthServiceProxy(
-        "http://%s:%s@127.0.0.1:51473" % (RPC_USER, RPC_KEY))
+@access_pivxd
+def pivx_get_staking_status(pivxd):
 
     include_properties = ["staking_status", "staking_enabled",
                           "coldstaking_enabled", "haveconnections", "mnsync", "walletunlocked", "stakeablecoins", "stakingbalance", "stakesplitthreshold"]
 
-    get_info = {key: value
-                for (key, value) in access.getstakingstatus().items()
-                if key in include_properties}
-    return get_info
+    return {key: value
+            for (key, value) in pivxd.getstakingstatus().items()
+            if key in include_properties}
 
 
-def pivx_get_wallet_info():
-    access = AuthServiceProxy(
-        "http://%s:%s@127.0.0.1:51473" % (RPC_USER, RPC_KEY))
+@access_pivxd
+def pivx_get_wallet_info(pivxd):
 
     include_properties = ["balance", "delegated_balance", "cold_staking_balance", "unconfirmed_balance",
                           "immature_balance", "immature_delegated_balance", "immature_cold_staking_balance", "txcount", "paytxfee"]
 
-    get_info = {key: value
-                for (key, value) in access.getwalletinfo().items()
-                if key in include_properties}
-    return get_info
+    return {key: value
+            for (key, value) in pivxd.getwalletinfo().items()
+            if key in include_properties}
 
 
-def pivx_get_mnsync_status():
+@access_pivxd
+def pivx_get_mnsync_status(pivxd):
     access = AuthServiceProxy(
         "http://%s:%s@127.0.0.1:51473" % (RPC_USER, RPC_KEY))
 
     include_properties = ["IsBlockchainSynced", "lastMasternodeList",
                           "lastMasternodeWinner", "lastFailure", "nCountFailures", "countMasternodeList"]
 
-    get_info = {key: value
-                for (key, value) in access.mnsync("status").items()
-                if key in include_properties}
-    return get_info
+    return {key: value
+            for (key, value) in pivxd.mnsync("status").items()
+            if key in include_properties}
